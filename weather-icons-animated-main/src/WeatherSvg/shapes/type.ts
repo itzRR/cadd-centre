e5970f76629077932669a0b007ff3cb3433c17b7
itzRR <rrbandara505@gmail.com>
2026-05-16 21:21:03 +0530

    chore: fix TypeScript missing declaration errors and clean up unused directory

--- weather-icons-animated-main/src/WeatherSvg/shapes/type.ts
+++
@@ -1,8 +0,0 @@
-import { SerializedStyles } from "@emotion/react";
-import { ReactElement, Fragment } from "react";
-
-export interface ShapeType {
-  styles: SerializedStyles;
-  defs?: ReactElement<typeof Fragment>;
-  symbol: ReactElement<"symbol">;
-}
